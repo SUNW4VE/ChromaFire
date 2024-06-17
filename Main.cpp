757f3709112b40ced0d2ce4d2044c6cdc710feb7
Andrew <155210303+Carimatic@users.noreply.github.com>
2024-06-17 15:00:14 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -11,6 +11,7 @@ void checkLastBind(int key0, int key1, int key2, int key3, int key4, int key5,
 int getBind(string input);
 void printBind(string categoryName, char standardBind);
 void printBind(string categoryName, string customBind);
+bool toggleIntercept(int toggleBind);
 
 int main() {
 
@@ -18,7 +19,7 @@ int main() {
 	SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
 
 	cout << "Launched ChromaFire ver. 2.0. \n\nPlease set ADS mode to \"Hold\" in-game.\n"
-		<< "Detection is on whenever the game window is focused and the toggle is switched to ON.\n"
+		<< "Detection is on whenever the game window is focused and the toggle bind is held down.\n"
 		<< "Set toggle to OFF and press Ctrl + Q to exit ChromaFire.\n";
 
 	// DETERMINE RESOLUTION & CENTER PIXEL
@@ -31,10 +32,13 @@ int main() {
 	const int CENTER_Y = (GetSystemMetrics(SM_CYSCREEN) / 2) - 3;
 	cout << "\nAuto-detected Windows resolution: " << (CENTER_X + 3) * 2 << "x" << (CENTER_Y + 3) * 2 << endl; // RESLOG
 
-	char toggleBind = '\0';
+	int triggerBind = '0';
 	int rThreshold = NULL;
 	int gbTolerance = NULL;
-	Weapon weapon[6] = { 
+	string triggerMode = "";
+	int switchInterval = NULL;
+
+	Weapon weapon[6] = {
 		Weapon("PRIMARY"),	// [0]
 		Weapon("BACKUP"),	// [1]
 		Weapon("MELEE"),	// [2]
@@ -51,30 +55,45 @@ int main() {
 	if (infile.is_open()) {
 
 		// filter out header/info reads
-		for (int i = 0; i < 12; i++) {
+		for (int i = 0; i < 15; i++) {
 			string infileDump;
 			infile >> infileDump;
 		}
 
-		// get toggleBind
-		string tempBind;
-		infile >> tempBind;
-		if (tempBind == "LCLICK" || tempBind == "RCLICK") {
-			cout << "\nERROR (3): toggleBind - Do NOT set the toggle bind to a mouse click button. Exiting ChromaFire.\n";
+		// get trigger mode
+		infile >> triggerMode;
+
+		if (!(triggerMode == "HOLD" || triggerMode == "TOGGLE")) {
+			cout << "\n\nERROR (4): toggleMode - Please set toggleMode to \"HOLD\" or \"TOGGLE\". Exiting ChromaFire.\n";
 			system("pause");
-			return 3;
-		}
-		else {
-			cout << "\n >> Toggle detection: [" << tempBind << "]";
-			toggleBind = getBind(tempBind);
+			return 4;
 		}
 
 		// filter out header/info reads
-		for (int i = 0; i < 16; i++) {
+		for (int i = 0; i < 6; i++) {
 			string infileDump;
 			infile >> infileDump;
 		}
-		
+
+		// get triggerBind
+		string tempBind;
+		infile >> tempBind;
+		if (tempBind == "RCLICK" || tempBind == "LCLICK") {
+			cout << "\n\nERROR (5): toggleBind - Do not set the toggle bind to left or right click. Exiting ChromaFire.\n";
+			system("pause");
+			return 5;
+		}
+
+		cout << "\n >> Trigger detection: [" << tempBind << "]";
+		cout << "\n >> Trigger Mode: " << triggerMode;
+		triggerBind = getBind(tempBind);
+
+		// filter out header/info reads
+		for (int i = 0; i < 24; i++) {
+			string infileDump;
+			infile >> infileDump;
+		}
+
 		// get weapon binds
 		for (int i = 0; i < 6; i++) {
 
@@ -89,7 +108,7 @@ int main() {
 			// it is normal that uppercase version of the letter isn't printed in terminal,
 			// because we are using tempBind instead of loadoutBind[i] as the print argument
 			printBind(weapon[i].getCategoryName(), tempBind);
-			
+
 		}
 
 		// filter out header/info reads
@@ -98,8 +117,8 @@ int main() {
 			infile >> infileDump;
 		}
 
-		// save detection intervals
-		cout << "\n\nFetching weapon detection intervals...";
+		// get detection intervals
+		cout << "\n\nFetching weapon detection intervals from config file...";
 		for (int i = 0; i < 6; i++) {
 
 			string infileDump;
@@ -111,22 +130,22 @@ int main() {
 				weapon[i].setInterval(tempInterval);
 			}
 			else {
-				cout << "\nERROR (4): weapon.setInterval() - Please ensure that all detection intervals "
+				cout << "\nERROR (3): weapon.setInterval() - Please ensure that all detection intervals "
 					<< "are set to 0 or higher. Exiting ChromaFire.\n";
 				system("pause");
-				return 4;
+				return 3;
 			}
 
 			cout << "\n - " << weapon[i].getCategoryName() << ": " << weapon[i].getInterval() << " ms";
 		}
 
 		// filter out header/info reads
-		for (int i = 0; i < 57; i++) {
+		for (int i = 0; i < 43; i++) {
 			string infileDump;
 			infile >> infileDump;
 		}
 
-		cout << "\n\nFetching thresholds and tolerances...";
+		cout << "\n\nFetching thresholds and tolerances from config file...";
 		// fetch red tolerance
 		infile >> rThreshold;
 		cout << "\n - Red Threshold: " << rThreshold << "/255";
@@ -140,6 +159,15 @@ int main() {
 		infile >> gbTolerance;
 		cout << "\n - Green/Blue Tolerance: " << gbTolerance << "/255";
 
+		for (int i = 0; i < 27; i++) {
+			string infileDump;
+			infile >> infileDump;
+		}
+
+		cout << "\n\nFetching weapon switch interval from config file...";
+		// fetch switch interval
+		infile >> switchInterval;
+		cout << "\n - Switch Interval: " << switchInterval << "ms";
 
 		infile.close();
 	}
@@ -148,6 +176,12 @@ int main() {
 		system("pause");
 		return 2;
 	}
+
+	/* DONE WITH CONFIG FETCHING */ /* DONE WITH CONFIG FETCHING */ /* DONE WITH CONFIG FETCHING */ 
+	/* DONE WITH CONFIG FETCHING */ /* DONE WITH CONFIG FETCHING */ /* DONE WITH CONFIG FETCHING */ 
+	/* DONE WITH CONFIG FETCHING */ /* DONE WITH CONFIG FETCHING */ /* DONE WITH CONFIG FETCHING */ 
+	/* DONE WITH CONFIG FETCHING */ /* DONE WITH CONFIG FETCHING */ /* DONE WITH CONFIG FETCHING */ 
+	/* DONE WITH CONFIG FETCHING */ /* DONE WITH CONFIG FETCHING */ /* DONE WITH CONFIG FETCHING */ 
 
 	// now that config fetching is done, get window handle
 	cout << "\n\nSearching for running game instance...";
@@ -158,80 +192,202 @@ int main() {
 		Sleep(500);
 	}
 	cout << "\nFound \"Pixel Gun 3D\" game instance.\n";
-	
+
 	// PRINCIPAL SECTION OF PROGRAM - detection + fire loop
 	int lastBind = '\0';
-	int lastBindCategoryNumber = NULL;
+	int newLastBindCategoryNumber = NULL;
+	int currentLastBindCategoryNumber = 0;
 	static HDC crosshair = NULL;
 	int r, g, b = NULL;
 	bool toggleState = false;
 	bool wasPressed = false;
+	bool shouldBreak = true;
 	// int detectionCount = 0; // used for performance measurements
 
-	while (!((GetAsyncKeyState(VK_CONTROL) & 0x8000) && (GetAsyncKeyState('Q') & 0x8000))) {
-		
-		// split all of these conditions to avoid unnecessary checking
-		while (GetForegroundWindow() == hwnd) {
+	if (triggerMode == "HOLD") {
 
-			// keep tracking lastBind to later determine what fire type to call
-			checkLastBind(weapon[0].weaponBind, weapon[1].weaponBind,
-				weapon[2].weaponBind, weapon[3].weaponBind, weapon[4].weaponBind, weapon[5].weaponBind,
-				lastBind, lastBindCategoryNumber);
+		while (!((GetAsyncKeyState(VK_CONTROL) & 0x8000) && (GetAsyncKeyState('Q') & 0x8000))) {
 
-			// check if toggle bind is currently pressed
-			if (GetAsyncKeyState(toggleBind)) {
+			// split all of these conditions to avoid unnecessary checking
+			while (GetForegroundWindow() == hwnd) {
 
-				// if the key was not pressed before, switch the toggle state
-				if (!wasPressed) {
+				// keep tracking lastBind and changing newLastBindCategoryNumber to determine what fire type to call
+					checkLastBind(weapon[0].weaponBind, weapon[1].weaponBind,
+						weapon[2].weaponBind, weapon[3].weaponBind, weapon[4].weaponBind, weapon[5].weaponBind,
+						lastBind, newLastBindCategoryNumber);
 
-					toggleState = !toggleState;
+				while (GetAsyncKeyState(triggerBind)) {
 
-					// log toggle state switches
-					if (toggleState) {
-						cout << "\nSet Toggle: ON";
-					}
-					else {
-						cout << "\nSet Toggle: OFF";
+					if (currentLastBindCategoryNumber != newLastBindCategoryNumber) {
+						currentLastBindCategoryNumber = newLastBindCategoryNumber;
+						std::this_thread::sleep_for(std::chrono::milliseconds(switchInterval));
 					}
 
-					wasPressed = true;
+					// sniper autoscope case
+					if (newLastBindCategoryNumber == 4) {
+						if (weapon[4].chromaSearch(CENTER_X, CENTER_Y, r, g, b, rThreshold, gbTolerance, crosshair)) {
 
-				}
-			}
-			else {
+							// cout << "\nzoomFire() Click!";
+							// cout << " | chromaFire pass #" << ++detectionCount; // measure the speed of which chromaSearches happen
+							weapon[4].zoomFire();
 
-				// if the key is NOT pressed, reset the wasPressed flag
-				wasPressed = false;
+							// while detection interval is taking place, check for weapon bind updates
+							auto duration = std::chrono::milliseconds(weapon[4].getInterval() - 10);
+							auto start = std::chrono::steady_clock::now();
+							while ((std::chrono::steady_clock::now() - start) < duration) {
 
-			}
+								checkLastBind(weapon[0].weaponBind, weapon[1].weaponBind,
+									weapon[2].weaponBind, weapon[3].weaponBind, weapon[4].weaponBind, weapon[5].weaponBind,
+									lastBind, newLastBindCategoryNumber);
+								if (newLastBindCategoryNumber != 4) {
+									shouldBreak = true;
+									break;
+								}
+							}
 
-			// execute the chromaSearch & fire conditionals if the toggle state is true
-			if (toggleState) {
-				// if the last bind was the sniper bind
-				if (lastBindCategoryNumber == 4) {
-					if (weapon[4].chromaSearch(CENTER_X, CENTER_Y, r, g, b, rThreshold, gbTolerance, crosshair)) {
-						// cout << "\nzoomFire() Click!";
-						// cout << " | chromaFire pass #" << ++detectionCount; // measure the speed of which chromaSearches happen
-						weapon[lastBindCategoryNumber].zoomFire();
+							if (shouldBreak) {
+								shouldBreak = false;
+								break;
+							}
+						}
+					}
+					// otherwise
+					else if (weapon[newLastBindCategoryNumber].chromaSearch(CENTER_X, CENTER_Y, r, g, b, rThreshold, gbTolerance, crosshair)) {
+						
+						// cout << "\nfire() Click!";
+						weapon[newLastBindCategoryNumber].fire();
+
+						// while detection interval is taking place, check for weapon bind updates
+						// currentLastBindCategoryNumber = newLastBindCategoryNumber;
+						auto duration = std::chrono::milliseconds(weapon[newLastBindCategoryNumber].getInterval() - 10);
+						auto start = std::chrono::steady_clock::now();
+						while (std::chrono::steady_clock::now() - start < duration) {
+
+							checkLastBind(weapon[0].weaponBind, weapon[1].weaponBind,
+								weapon[2].weaponBind, weapon[3].weaponBind, weapon[4].weaponBind, weapon[5].weaponBind,
+								lastBind, newLastBindCategoryNumber);
+							if (currentLastBindCategoryNumber != newLastBindCategoryNumber) {
+								shouldBreak = true;
+								break;
+							}
+						}
+						
+						if (shouldBreak) {
+							shouldBreak = false;
+							break;
+						}
 					}
 				}
-				// otherwise
-				else if (weapon[lastBindCategoryNumber].chromaSearch(CENTER_X, CENTER_Y, r, g, b, rThreshold, gbTolerance, crosshair)) {
-					// cout << "\nfire() Click!";
-					weapon[lastBindCategoryNumber].fire();
-				}
+
+				this_thread::sleep_for(std::chrono::milliseconds(10));
+
 			}
 		}
 
-		this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+	else if (triggerMode == "TOGGLE" ) {
+
+		while (!((GetAsyncKeyState(VK_CONTROL) & 0x8000) && (GetAsyncKeyState('Q') & 0x8000))) {
+
+			// split all of these conditions to avoid unnecessary checking
+			while (GetForegroundWindow() == hwnd) {
+
+				// keep tracking lastBind to later determine what fire type to call
+				checkLastBind(weapon[0].weaponBind, weapon[1].weaponBind,
+					weapon[2].weaponBind, weapon[3].weaponBind, weapon[4].weaponBind, weapon[5].weaponBind,
+					lastBind, newLastBindCategoryNumber);
+
+				if (currentLastBindCategoryNumber != newLastBindCategoryNumber) {
+					currentLastBindCategoryNumber = newLastBindCategoryNumber;
+					std::this_thread::sleep_for(std::chrono::milliseconds(switchInterval));
+				}
+
+				// check if trigger bind is currently pressed
+				if (GetAsyncKeyState(triggerBind)) {
+
+					// if the key was not pressed before, switch the toggle state
+					if (!wasPressed) {
+
+						toggleState = !toggleState;
+
+						// log toggle state switches
+						if (toggleState) {
+							cout << "\nSet Toggle: ON";
+						}
+						else {
+							cout << "\nSet Toggle: OFF";
+						}
+
+						wasPressed = true;
+
+					}
+				}
+				else {
+					// if the key is NOT pressed, reset the wasPressed flag
+					wasPressed = false;
+				}
+
+				// execute the chromaSearch & fire conditionals if the toggle state is true
+				if (toggleState) {
+					// if the last bind was the sniper bind
+					if (newLastBindCategoryNumber == 4) {
+						if (weapon[4].chromaSearch(CENTER_X, CENTER_Y, r, g, b, rThreshold, gbTolerance, crosshair)) {
+
+							// cout << "\nzoomFire() Click!";
+							// cout << " | chromaFire pass #" << ++detectionCount; // measure the speed of which chromaSearches happen
+							weapon[4].zoomFire();
+
+							// while detection interval is taking place, check for weapon bind updates
+							auto duration = std::chrono::milliseconds(weapon[4].getInterval() - 10);
+							auto start = std::chrono::steady_clock::now();
+							while (std::chrono::steady_clock::now() - start < duration) {
+
+								checkLastBind(weapon[0].weaponBind, weapon[1].weaponBind,
+									weapon[2].weaponBind, weapon[3].weaponBind, weapon[4].weaponBind, weapon[5].weaponBind,
+									lastBind, newLastBindCategoryNumber);
+								if (newLastBindCategoryNumber != 4) {
+									break;
+								}
+							}
+						}
+					}
+					// otherwise
+					else if (weapon[newLastBindCategoryNumber].chromaSearch(CENTER_X, CENTER_Y, r, g, b, rThreshold, gbTolerance, crosshair)) {
+						
+						// cout << "\nfire() Click!";
+						weapon[newLastBindCategoryNumber].fire();
+
+						// while detection interval is taking place, check for weapon bind updates
+						currentLastBindCategoryNumber = newLastBindCategoryNumber;
+						auto duration = std::chrono::milliseconds(weapon[newLastBindCategoryNumber].getInterval() - 10);
+						auto start = std::chrono::steady_clock::now();
+						while (std::chrono::steady_clock::now() - start < duration) {
+
+							checkLastBind(weapon[0].weaponBind, weapon[1].weaponBind,
+								weapon[2].weaponBind, weapon[3].weaponBind, weapon[4].weaponBind, weapon[5].weaponBind,
+								lastBind, newLastBindCategoryNumber);
+							if (newLastBindCategoryNumber != currentLastBindCategoryNumber) {
+								break;
+							}
+						}
+					}
+				}
+			}
+
+			this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
 	}
 
 	::ReleaseDC(0, crosshair);
-	cout << "\nCtrl + Q was pressed. Exiting ChromaFire.\n";
+	cout << "\n\nCtrl + Q was pressed. Exiting ChromaFire.\n";
 	system("pause");
 	return 0;
 }
 
+// intercept toggle bind so that the native bind does not send through
+bool toggleIntercept(int toggleBind) {
+
+}
 
 // set last weapon bind, then return number of weapon
 void checkLastBind(int key0, int key1, int key2, int key3, int key4, int key5,
@@ -355,10 +511,10 @@ int getBind(string input) {
 	return input[0];
 }
 
-void printBind(string bindCategory, char standardKey) {
-	cout << "\n - " << bindCategory << ": [" << standardKey << "]";
+void printBind(string bindCategory, char standardBind) {
+	cout << "\n - " << bindCategory << ": [" << standardBind << "]";
 }
 
-void printBind(string bindCategory, string customKey) {
-	cout << "\n - " << bindCategory << ": [" << customKey << "]";
+void printBind(string bindCategory, string customBind) {
+	cout << "\n - " << bindCategory << ": [" << customBind << "]";
 }
